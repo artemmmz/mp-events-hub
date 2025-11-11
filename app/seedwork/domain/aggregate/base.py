65f6179adc9936b893fe0a437261fe7cdd8de271
wokie <contextyoo@gmail.com>
2025-11-11 17:36:25 +0300

    feat(aggregate): add base classes

---
+++ app/seedwork/domain/aggregate/base.py
@@ -0,0 +1,30 @@
+from abc import ABC
+from copy import copy
+from dataclasses import (
+    dataclass,
+    field,
+)
+
+from seedwork.domain.entities import TimestampEntity
+from seedwork.domain.event import DomainEvent
+
+
+@dataclass(
+    kw_only=True,
+    slots=True,
+)
+class BaseAggregate(
+    TimestampEntity,
+    ABC,
+):
+    _events: list[DomainEvent] = field(default_factory=list)
+
+    def register_event(self, event: DomainEvent) -> None:
+        self._events.append(event)
+
+    def pull_events(self) -> list[DomainEvent]:
+        registered_events = copy(self._events)
+
+        self._events.clear()
+
+        return registered_events
