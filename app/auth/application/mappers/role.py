8f9b1a8276518388fd047fcbe3e5610d66305cd4
wokie <contextyoo@gmail.com>
2025-11-13 04:05:34 +0300

    feat(auth): add mappers

---
+++ app/auth/application/mappers/role.py
@@ -0,0 +1,9 @@
+from auth.domain.value_object.roles import RoleValue
+from infra.pg.models.role import RoleOrm
+from seedwork.application.mapper import BaseMapper
+
+
+class RoleMapper(BaseMapper):
+    @staticmethod
+    def to_orm(role: RoleValue) -> RoleOrm:
+        return RoleOrm(name=role.value)
---
+++ app/auth/application/mappers/user.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+from auth.application.mappers.role import RoleMapper
+from auth.domain.entities.user import User
+from infra.pg.models.role import RoleOrm
+from infra.pg.models.user import UserOrm
+from seedwork.application.mapper import BaseMapper
+
+
+@dataclass
+class UserMapper(BaseMapper):
+    _role_mapper: RoleMapper
+
+    def to_orm(self, user: User) -> UserOrm:
+        role: RoleOrm = self._role_mapper.to_orm(role=user.role)
+
+        return UserOrm(
+            aggregate_id=user.id,
+            name=user.name.value,
+            second_name=user.second_name.value,
+            group_number=user.group_number.value,
+            email=user.email.value,
+            hash_password=user.hash_password,
+            role=role,
+        )
