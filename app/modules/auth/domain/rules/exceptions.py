74a1a0a5c2d169d7b8c604a047d862fb3e6ade93
wokie <contextyoo@gmail.com>
2025-11-22 14:06:51 +0300

    feat(auth): add exceptions

--- app/modules/auth/domain/rules/exceptions.py
+++ app/modules/auth/domain/rules/exceptions.py
@@ -25,3 +25,21 @@ class UserAlreadyExistsException(BusinessRuleException):
             f"User '{self.name.value} {self.second_name.value}' "
             f"from group '{self.group_number.value}' already exists."
         )
+
+
+@dataclass
+class UserNotFoundException(BusinessRuleException):
+    email: EmailValue
+
+    @property
+    def message(self) -> str:
+        return f"User with email '{self.email.value}' not found."
+
+
+@dataclass
+class InvalidPasswordException(BusinessRuleException):
+    email: EmailValue
+
+    @property
+    def message(self) -> str:
+        return f"Invalid password for user with email '{self.email.value}'."
