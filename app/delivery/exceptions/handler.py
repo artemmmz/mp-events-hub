98b6d834075e1bea7cdba1541913051eceebf9e5
wokie <contextyoo@gmail.com>
2025-11-22 14:45:13 +0300

    refactor(exceptions): add error handling

--- app/delivery/exceptions/handler.py
+++ app/delivery/exceptions/handler.py
@@ -8,8 +8,10 @@ from starlette.status import (
     HTTP_409_CONFLICT,
     HTTP_404_NOT_FOUND,
     HTTP_401_UNAUTHORIZED,
+    HTTP_400_BAD_REQUEST,
 )
 
+from seedwork.domain.value_objects.common.exceptions import ValueException
 from seedwork.exceptions import AppException
 from modules.auth.domain.rules.exceptions import (
     UserAlreadyExistsException,
@@ -28,9 +30,18 @@ exceptions_map: dict[type[AppException], int] = {
     InvalidPasswordException: HTTP_401_UNAUTHORIZED,
 }
 
+
 def register_exception_handlers(app: FastAPI) -> None:
     @app.exception_handler(AppException)
     async def handler(request: Request, exc: AppException) -> None:
-        if exceptions_map.get(type(exc), None):
-            status: int | None = exceptions_map.get(type(exc))
+        # Если ошибка наследуется от ValueException
+        if isinstance(exc, ValueException):
+            raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=exc.message)
+
+        # Смотрим на остальные конкретные ошибки
+        status: int | None = exceptions_map.get(type(exc))
+        if status:
             raise HTTPException(status_code=status, detail=exc.message)
+
+        # Для всех остальных AppException
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail=exc.message)
