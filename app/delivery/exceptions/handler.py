3fe8b2acd8758010ae57df26d69beb2b55c6883f
wokie <contextyoo@gmail.com>
2025-11-22 14:23:11 +0300

    fix(delivery/exception_handler): add HTTP 401 status

--- app/delivery/exceptions/handler.py
+++ app/delivery/exceptions/handler.py
@@ -6,12 +6,16 @@ from fastapi import (
 from starlette.status import (
     HTTP_500_INTERNAL_SERVER_ERROR,
     HTTP_409_CONFLICT,
+    HTTP_404_NOT_FOUND,
+    HTTP_401_UNAUTHORIZED,
 )
 
 from seedwork.exceptions import AppException
 from modules.auth.domain.rules.exceptions import (
     UserAlreadyExistsException,
     EmailAlreadyExistsException,
+    UserNotFoundException,
+    InvalidPasswordException,
 )
 
 
@@ -20,6 +24,8 @@ exceptions_map: dict[type[AppException], int] = {
     AppException: HTTP_500_INTERNAL_SERVER_ERROR,
     UserAlreadyExistsException: HTTP_409_CONFLICT,
     EmailAlreadyExistsException: HTTP_409_CONFLICT,
+    UserNotFoundException: HTTP_404_NOT_FOUND,
+    InvalidPasswordException: HTTP_401_UNAUTHORIZED,
 }
 
 def register_exception_handlers(app: FastAPI) -> None:
