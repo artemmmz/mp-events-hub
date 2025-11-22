441475e11beffb351553c0e73eefa34d830d1ba9
wokie <contextyoo@gmail.com>
2025-11-22 14:16:18 +0300

    feat(auth/api): add login and register handlers

--- app/delivery/api/v1/auth/handlers.py
+++ app/delivery/api/v1/auth/handlers.py
@@ -4,16 +4,25 @@ from dishka.integrations.fastapi import (
     inject,
 )
 from fastapi import APIRouter
-from starlette.status import HTTP_201_CREATED
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+)
 from fastapi.responses import Response
 
 from delivery.api.v1.auth.schemas import (
     RegisterInSchema,
     RegisterOutSchema,
+    LoginInSchema,
+    LoginOutSchema,
 )
 from modules.auth.application.use_cases.register import (
     RegisterUseCase,
     RegisterCommand,
+)
+from modules.auth.application.use_cases.login import (
+    LoginUseCase,
+    LoginCommand,
 )
 from seedwork.domain.value_objects.jwt import JwtTokenValue
 
@@ -55,5 +64,37 @@ async def register(
         samesite="lax",
         expires="Wed, 31 Dec 2137 23:59:59 GMT",
     )
+
+    return schema
+
+
+@router.post(
+    path="/login",
+    response_model=LoginOutSchema,
+    status_code=HTTP_200_OK,
+    summary="",
+)
+@inject
+async def login(
+    schema: LoginInSchema,
+    response: Response,
+    use_case: FromDishka[LoginUseCase],
+) -> LoginOutSchema:
+    command = LoginCommand(
+        email=schema.email,
+        password=schema.password,
+    )
+
+    token: JwtTokenValue = await use_case.act(command=command)
+
+    response.set_cookie(
+        key="access_token",
+        value=token.value,
+        httponly=True,
+        samesite="lax",
+        expires="Wed, 31 Dec 2137 23:59:59 GMT",
+    )
+
+    schema = LoginOutSchema(token=token.value)
 
     return schema
