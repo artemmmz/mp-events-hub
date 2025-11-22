e21791e9aa14872a4e786d097256541fd4d88ae6
wokie <contextyoo@gmail.com>
2025-11-22 14:10:15 +0300

    feat(auth/ioc): add login use case

--- app/bootstrap/ioc/providers/modules/auth/application/use_case.py
+++ app/bootstrap/ioc/providers/modules/auth/application/use_case.py
@@ -1,6 +1,7 @@
 from dishka import Provider, Scope, provide
 
 from modules.auth.application.services.jwt import JwtService
+from modules.auth.application.use_cases.login import LoginUseCase
 from modules.auth.application.use_cases.register import RegisterUseCase
 from modules.auth.domain.repository.user import IUserRepository
 from modules.auth.domain.rules.user import UniqueEmailRule, UniqueUserRule
@@ -25,4 +26,15 @@ class UseCaseAuthProvider(Provider):
             _unique_email_rule=unique_email_rule,
             _unique_user_rule=unique_user_rule,
             _jwt_manager=jwt_manager,
+        )
+
+    @provide
+    def login(
+        self,
+        user_repo: IUserRepository,
+        jwt_service: JwtService,
+    ) -> LoginUseCase:
+        return LoginUseCase(
+            _user_repo=user_repo,
+            _jwt_service=jwt_service,
         )
