6b182e212413254c260c495a6b1516d18cc9bbca
wokie <contextyoo@gmail.com>
2025-11-22 03:48:43 +0300

    feat(auth): add role data mappers

---
+++ app/modules/auth/infra/dm/role.py
@@ -0,0 +1,21 @@
+from sqlalchemy import select
+
+from infra.pg.models import RoleOrm
+from modules.auth.application.interface.dm import IRoleDm
+from modules.auth.domain.value_object.roles import RoleValue
+from seedwork.infra.dm.alchemy import BaseAlchemyDataMapper
+
+
+class RoleAlchemyDm(
+    IRoleDm,
+    BaseAlchemyDataMapper,
+):
+    async def get_by_name(self, role: RoleValue) -> RoleOrm | None:
+        result = await self._session.execute(
+            select(RoleOrm)
+            .where(RoleOrm.name == role.value)
+        )
+
+        role_orm: RoleOrm | None = result.scalar_one_or_none()
+
+        return role_orm
