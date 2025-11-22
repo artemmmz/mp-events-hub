837f0d1fe25b46c33a2dd710ad2522bbb4306c22
wokie <contextyoo@gmail.com>
2025-11-22 06:02:44 +0300

    feat(handler): add conflict exceptions

--- app/delivery/exceptions/handler.py
+++ app/delivery/exceptions/handler.py
@@ -5,12 +5,21 @@ from fastapi import (
 )
 from starlette.status import (
     HTTP_500_INTERNAL_SERVER_ERROR,
+    HTTP_409_CONFLICT,
 )
 
 from seedwork.exceptions import AppException
+from modules.auth.domain.rules.exceptions import (
+    UserAlreadyExistsException,
+    EmailAlreadyExistsException,
+)
+
 
 exceptions_map: dict[type[AppException], int] = {
+
     AppException: HTTP_500_INTERNAL_SERVER_ERROR,
+    UserAlreadyExistsException: HTTP_409_CONFLICT,
+    EmailAlreadyExistsException: HTTP_409_CONFLICT,
 }
 
 def register_exception_handlers(app: FastAPI) -> None:
