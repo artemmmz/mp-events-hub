a24e00d3eb93ce7e3718bdfcf87209b6226b4b65
wokie <contextyoo@gmail.com>
2025-11-29 14:33:26 +0300

    feat(event_map): add RegistrationRequestedUserEvent queue mapping

--- app/bootstrap/event_map.py
+++ app/bootstrap/event_map.py
@@ -1,6 +1,9 @@
 from seedwork.domain.event import DomainEvent
 from seedwork.infra.event_bus.base import QueueRmq
 
-event_queue_map: dict[type[DomainEvent], QueueRmq] = {
+from modules.auth.domain.events import RegistrationRequestedUserEvent
+from modules.email.infra.rmq.queues import REGISTRATION_REQUESTED_USER_EVENT_QUEUE
 
+event_queue_map: dict[type[DomainEvent], QueueRmq] = {
+    RegistrationRequestedUserEvent: REGISTRATION_REQUESTED_USER_EVENT_QUEUE,
 }
