f48795d1fe02c32c2445d24496d8d990edfff818
wokie <contextyoo@gmail.com>
2025-11-29 14:30:14 +0300

    fix(user_mapper): extract value objects

--- app/modules/auth/application/mappers/user.py
+++ app/modules/auth/application/mappers/user.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 
-from modules.auth.domain.entities.user import User
+from modules.auth.domain.aggregate.user import User
 from infra.pg.models.user import UserOrm
 from modules.auth.domain.value_object.roles import RoleValue
 from seedwork.application.mapper import BaseMapper
+from seedwork.domain.value_objects.common.entity import EntityIdValue
+from seedwork.domain.value_objects.user import NameValue, GroupNumberValue, EmailValue
 
 
 @dataclass
@@ -11,21 +13,26 @@ class UserMapper(BaseMapper):
     @staticmethod
     def to_orm(user: User) -> UserOrm:
         return UserOrm(
-            uid=user.id,
+            uid=user.id.value,
             name=user.name.value,
             second_name=user.second_name.value,
             group_number=user.group_number.value,
             email=user.email.value,
             hash_password=user.hash_password,
+            email_confirm=user.email_confirm,
         )
 
     @staticmethod
     def to_entity(user_orm: UserOrm) -> User:
-        return User.create(
-            name=user_orm.name,
-            second_name=user_orm.second_name,
-            group_number=user_orm.group_number,
-            email=user_orm.email,
-            password=user_orm.hash_password,
-            role=RoleValue(user_orm.role.name)
+        return User(
+            id=EntityIdValue(_value=user_orm.uid),
+            _created_at=user_orm.created_at,
+            _updated_at=user_orm.updated_at,
+            name=NameValue(_value=user_orm.name),
+            second_name=NameValue(_value=user_orm.second_name),
+            group_number=GroupNumberValue(_value=user_orm.group_number),
+            email=EmailValue(_value=user_orm.email),
+            hash_password=user_orm.hash_password,
+            role=RoleValue(user_orm.role.name),
+            email_confirm=user_orm.email_confirm,
         )
