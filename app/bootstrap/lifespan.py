4f9ebe10d029083e8a6a9a0b97e63fc95a14dc62
wokie <contextyoo@gmail.com>
2025-11-29 14:34:26 +0300

    refactor(lifespan): add FastStream entrypoint

--- app/bootstrap/lifespan.py
+++ app/bootstrap/lifespan.py
@@ -1,9 +1,33 @@
+import asyncio
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
+from dishka import AsyncContainer
 from fastapi import FastAPI
+from faststream import FastStream
+from faststream.rabbit import RabbitBroker
+
+from bootstrap.ioc import get_container
+from delivery.api.events import router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    yield
-    await app.state.dishka_container.close()
+    container: AsyncContainer = get_container()
+
+    async with container() as cont:
+        faststream: FastStream = await cont.get(FastStream)
+        broker: RabbitBroker = await cont.get(RabbitBroker)
+        broker.include_router(router)
+
+        task = asyncio.create_task(faststream.run())
+
+        yield
+
+        task.cancel()
+        try:
+            await task
+        except asyncio.CancelledError:
+            pass
+
+        await app.state.dishka_container.close()
