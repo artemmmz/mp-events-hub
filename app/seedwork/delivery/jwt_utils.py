bded7eff9b629aefd4e93ac24c745630ebe5c79c
wokie <contextyoo@gmail.com>
2025-12-02 00:22:55 +0300

    feat(seedwork/auth): add jwt utils

---
+++ app/seedwork/delivery/jwt_utils.py
@@ -0,0 +1,33 @@
+from typing import Any
+from uuid import UUID
+
+import jwt
+from fastapi import HTTPException, Cookie
+
+from bootstrap.settings import get_settings
+
+
+def get_user_id(
+    access_token: str | None = Cookie(default=None)
+) -> UUID:
+    settings = get_settings()
+
+    if access_token is None:
+        raise HTTPException(status_code=401, detail="No access token")
+
+    try:
+        payload: dict[str, Any] = jwt.decode(
+            access_token,
+            settings.auth.secret_key,
+            algorithms=[settings.auth.algorithm],
+        )
+
+    except jwt.InvalidTokenError:
+        raise # todo дописать Exception
+
+    user_id: UUID | None = payload.get("user_id", None)
+
+    if user_id:
+        return user_id
+
+    raise # todo дописать Exception
