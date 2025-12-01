2dad8432d8b9a1dc61c295b3e682c55024ba64b0
wokie <contextyoo@gmail.com>
2025-12-02 00:22:00 +0300

    refact(models): add RoleOrm and EventOrm imports

--- app/seedwork/infra/pg/models/__init__.py
+++ app/seedwork/infra/pg/models/__init__.py
@@ -1,10 +1,20 @@
 from .role import RoleOrm
-from .user import UserOrm
 from .common import BaseOrm
+from modules.auth.infra.pg.models import UserAuthOrm as UserAuthOrm
+from modules.event.infra.pg.models import (
+    EventOrm,
+    AddressOrm,
+    BuildingOrm,
+    UserEventOrm as UserEventOrm,
+)
 
 
 __all__ = (
     "BaseOrm",
     "RoleOrm",
-    "UserOrm",
+    "UserAuthOrm",
+    "EventOrm",
+    "AddressOrm",
+    "BuildingOrm",
+    "UserEventOrm",
 )
