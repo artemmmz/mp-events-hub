b4fe4cd2888543252f90eb23150edec9c10ada45
wokie <contextyoo@gmail.com>
2025-12-02 00:30:29 +0300

    feat(repository): add event and user repositories

---
+++ app/modules/event/domain/repository/user.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+from modules.event.domain.aggregate.user import User
+from seedwork.domain.repository import BaseRepository
+from seedwork.domain.value_objects.common.entity import EntityIdValue
+
+
+class IUserRepository(
+    BaseRepository,
+    ABC,
+):
+    @abstractmethod
+    async def get_by_id(self, required_id: EntityIdValue) -> User:
+        ...
+
+    @abstractmethod
+    async def create(self, user: User) -> None:
+        ...
