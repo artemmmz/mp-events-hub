c5e9fb5877ef92a946db9a778273046b641a62e3
wokie <contextyoo@gmail.com>
2025-12-02 00:24:59 +0300

    refactor(seedwork/dm): from /auth/interface/dm -> seedwork/interface/dm

--- app/seedwork/application/interface/dm/sql/roles.py
+++ app/seedwork/application/interface/dm/sql/roles.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
-from infra.pg.models import RoleOrm
-from seedwork.domain.value_object.user import RoleValue
+from seedwork.infra.pg.models import RoleOrm
+from seedwork.domain.value_objects.role import RoleValue
 from seedwork.infra.dm.base import BaseDataMapper
 
 
