23a4aa43f7711850d08c39fc28594e4169d6e5f0
wokie <contextyoo@gmail.com>
2025-12-02 21:26:48 +0300

    feat(event/rules): add user registration rule

---
+++ app/modules/event/domain/rules/exceptions.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from seedwork.domain.rules import BusinessRuleException
+from seedwork.domain.value_objects.common.entity import EntityIdValue
+
+
+@dataclass
+class UserAlreadyRegisteredException(BusinessRuleException):
+    user_id: EntityIdValue
+    event_id: EntityIdValue
+
+    @property
+    def message(self) -> str:
+        return (
+            f"Пользователь '{self.user_id}' уже зарегистрирован на событие '{self.event_id}'."
+        )
