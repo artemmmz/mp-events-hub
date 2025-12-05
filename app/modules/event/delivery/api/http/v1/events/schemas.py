8832893f3014d3500b2b2c8a7ba480dac55252aa
wokie <contextyoo@gmail.com>
2025-12-05 22:38:27 +0300

    refactor(event/api): add update-event API handlers

--- app/modules/event/delivery/api/http/v1/events/schemas.py
+++ app/modules/event/delivery/api/http/v1/events/schemas.py
@@ -1,21 +1,39 @@
 from datetime import datetime
+from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel
+
+from seedwork.domain.marker import EMPTY
 
 
 class CreateEventInSchema(BaseModel):
     title: str
     scheduled_at: datetime
     description: str
-    city: str
-    street: str
-    building_number: int
-    block: str | None
-    auditorium: str | None
+    city: Optional[str] = None
+    street: Optional[str] = None
+    building_number: Optional[int] = None
+    block: Optional[str] = None
+    auditorium: Optional[str] = None
 
 
 class CreateEventOutSchema(BaseModel):
+    event_id: UUID
+
+
+class UpdateEventInSchema(BaseModel):
+    title: Optional[str] = EMPTY
+    scheduled_at: Optional[datetime] = EMPTY
+    description: Optional[str] = EMPTY
+    city: Optional[str] = EMPTY
+    street: Optional[str] = EMPTY
+    building_number: Optional[int] = EMPTY
+    block: Optional[str] = EMPTY
+    auditorium: Optional[str] = EMPTY
+
+
+class UpdateEventOutSchema(BaseModel):
     event_id: UUID
 
 
