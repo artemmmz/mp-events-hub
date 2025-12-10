f7d16a9bb07ff07bc1ec415f970f40bc12d2741d
wokie <contextyoo@gmail.com>
2025-12-11 01:32:51 +0300

    fix(auth/use-case): handle user not found

--- app/modules/auth/application/use_cases/confirm_reset.py
+++ app/modules/auth/application/use_cases/confirm_reset.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 
 from modules.auth.application.interface.dm.kvalue.user import IUserKvDm
+from modules.auth.application.services.jwt import JwtService
 from modules.auth.domain.aggregate.user import User
 from modules.auth.domain.repository.user import IUserRepository
+from modules.auth.domain.rules.exceptions import UserNotFoundException
+from modules.auth.domain.value_object.confirm_code import ConfirmCodeValue
 from seedwork.application.use_case import BaseUseCase
 from seedwork.domain.value_objects.jwt import JwtTokenValue
 from seedwork.domain.value_objects.user import EmailValue
@@ -19,6 +22,7 @@ class ConfirmResetPasswordCommand:
 class ConfirmResetPasswordUseCase(
     BaseUseCase[ConfirmResetPasswordCommand, JwtTokenValue],
 ):
+    _jwt_service: JwtService
     _user_repo: IUserRepository
     _user_dm: IUserKvDm
     _transactional_manager: ITransactionManager
@@ -27,8 +31,28 @@ class ConfirmResetPasswordUseCase(
         self,
         command: ConfirmResetPasswordCommand,
     ) -> JwtTokenValue:
-        user: User = await self._user_repo.find_by_email(
+        user: User | None = await self._user_repo.find_by_email(
             email=EmailValue(command.email),
         )
 
-        user.reset_password
+        if not user:
+            raise UserNotFoundException(email=command.email)
+
+        confirm_code: str | None = await self._user_dm.get_confirm_code_by_email(
+            email=EmailValue(command.email),
+        )
+        new_password: str | None = await self._user_dm.get_password_by_email(
+            email=EmailValue(command.email),
+        )
+
+        user.reset_password(
+            new_password=new_password,
+            input_code=ConfirmCodeValue(command.confirm_code),
+            stored_code=ConfirmCodeValue(confirm_code),
+        )
+
+        token: JwtTokenValue = self._jwt_service.issue_token(
+            payload={"user_id": user.id.value},
+        )
+
+        return token
