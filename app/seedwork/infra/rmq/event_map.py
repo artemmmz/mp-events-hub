7deee207e8f77e7ff2dc55208f48b8d897e134fb
wokie <contextyoo@gmail.com>
2025-12-11 23:37:44 +0300

    feat(seedwork/event_map): add reset password event

--- app/seedwork/infra/rmq/event_map.py
+++ app/seedwork/infra/rmq/event_map.py
@@ -4,13 +4,16 @@ from seedwork.infra.event_bus.base import QueueRmq
 from seedwork.domain.events.auth import (
     RequestedRegistrationUserEvent,
     ConfirmRegistrationUserEvent,
+    RequestResetPasswordEvent,
 )
 from seedwork.infra.rmq.queues import (
     REQUESTED_REGISTRATION_USER_EVENT_QUEUE,
     CONFIRM_REGISTRATION_USER_EVENT_QUEUE,
+    REQUESTED_RESET_PASSWORD_USER_EVENT_QUEUE,
 )
 
 event_queue_map: dict[type[DomainEvent], QueueRmq] = {
     RequestedRegistrationUserEvent: REQUESTED_REGISTRATION_USER_EVENT_QUEUE,
     ConfirmRegistrationUserEvent: CONFIRM_REGISTRATION_USER_EVENT_QUEUE,
+    RequestResetPasswordEvent: REQUESTED_RESET_PASSWORD_USER_EVENT_QUEUE
 }
