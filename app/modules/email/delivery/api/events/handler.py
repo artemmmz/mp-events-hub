a641ca8d74702d88a9dcb8d27e806af5aa4d77bb
wokie <contextyoo@gmail.com>
2025-12-11 23:38:06 +0300

    feat(auth/email_handler): add reset password handler

--- app/modules/email/delivery/api/events/handler.py
+++ app/modules/email/delivery/api/events/handler.py
@@ -1,12 +1,19 @@
 from faststream.rabbit import RabbitRouter
 
 from bootstrap.ioc import get_container
-from seedwork.domain.events.auth import RequestedRegistrationUserEvent
+from seedwork.domain.events.auth import (
+    RequestedRegistrationUserEvent,
+    RequestResetPasswordEvent,
+)
 from modules.email.application.send_confirm_code import (
     SendConfirmCodeUseCase,
     SendConfirmCodeCommand,
 )
-from seedwork.infra.rmq.queues import REQUESTED_REGISTRATION_USER_EVENT_QUEUE
+from seedwork.infra.rmq.queues import (
+    REQUESTED_REGISTRATION_USER_EVENT_QUEUE,
+    REQUESTED_RESET_PASSWORD_USER_EVENT_QUEUE,
+)
+
 
 router = RabbitRouter()
 
@@ -14,8 +21,28 @@ router = RabbitRouter()
 @router.subscriber(
     queue=REQUESTED_REGISTRATION_USER_EVENT_QUEUE,
 )
-async def send_verification_code(
+async def requested_registration_user(
     event: RequestedRegistrationUserEvent,
+) -> None:
+    container = get_container()
+
+    async with container() as cont:
+        use_case: SendConfirmCodeUseCase = await cont.get(SendConfirmCodeUseCase)
+
+        command = SendConfirmCodeCommand(
+            email=event.email,
+            confirm_code=event.confirm_code,
+            subject="Код подтверждения, Московский Политех"
+        )
+
+        await use_case.act(command=command)
+
+
+@router.subscriber(
+    queue=REQUESTED_RESET_PASSWORD_USER_EVENT_QUEUE,
+)
+async def requested_reset_password(
+    event: RequestResetPasswordEvent,
 ) -> None:
     container = get_container()
 
